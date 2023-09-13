11335551c3d3328201c0931a33b713367d5c4cca
Tu Vu <vuanhtu52@gmail.com>
2023-09-13 16:34:07 +0700

    Add sidebar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import "./style.css";
 import createNavBar from "./navBar/navBar";
+import createSideBar from "./sideBar/sideBar";
 
 const loadPage = () => {
     const body = document.querySelector("body");
 
+    // Add navbar
     const navBar = createNavBar();
     body.appendChild(navBar);
+
+    // Add sidebar
+    const sideBar = createSideBar();
+    body.appendChild(sideBar);
+
+    // Add main content
+    const content = document.createElement("div");
+    content.textContent = "content"
+    body.appendChild(content);
 }
 
 loadPage();
---
+++ src/sideBar/sideBar.js
@@ -0,0 +1,56 @@
+import "./sideBar.css";
+import InboxIconLink from "./inbox.svg";
+import TodayIconLink from "./today.svg";
+import UpCompingIconLink from "./upcoming.svg";
+
+const createItem = (imageLink, text) => {
+    const item = document.createElement("div");
+    item.className = "sidebar-item";
+    
+    const image = new Image();
+    image.src = imageLink;
+    item.appendChild(image);
+
+    const itemName = document.createElement("div");
+    itemName.textContent = text;
+    item.appendChild(itemName);
+
+    return item;
+} 
+
+const createProjectsHeader = () => {
+    const header = document.createElement("div");
+    header.className = "projects-header";
+
+    const name = document.createElement("div")
+    name.textContent = "Projects";
+    header.appendChild(name);
+
+    const addProjectButton = document.createElement("button");
+    addProjectButton.textContent = "+";
+    header.appendChild(addProjectButton);
+
+    return header;
+}
+
+const createSideBar = () => {
+    const sideBar = document.createElement("div");
+    sideBar.className = "side-bar";
+
+    // Add default tabs at the top
+    const defaultItems = document.createElement("div");
+    defaultItems.className = "default-items";
+    defaultItems.appendChild(createItem(InboxIconLink, "Inbox"));
+    defaultItems.appendChild(createItem(TodayIconLink, "Today"));
+    defaultItems.appendChild(createItem(UpCompingIconLink, "Upcoming"));
+    sideBar.appendChild(defaultItems);
+
+    // Projects section
+    const projects = document.createElement("div");
+    projects.appendChild(createProjectsHeader());
+    sideBar.appendChild(projects);
+
+    return sideBar;
+}
+
+export default createSideBar;
